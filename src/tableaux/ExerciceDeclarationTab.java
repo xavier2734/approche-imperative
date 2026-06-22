fdc021687ab8eb2ab3ad16b376286c524bc2cc16
xavier <xavier.vigneron34@gmail.com>
2026-06-22 15:36:29 +0200

    v1

---
+++ src/tableaux/ExerciceDeclarationTab.java
@@ -0,0 +1,15 @@
+package tableaux;
+
+public class ExerciceDeclarationTab {
+    public static void main(String[] args) {
+
+
+        int[] chiffres = {8, -7, 12, 1, -2, 14, 17, 9};
+        System.out.println(chiffres);
+        System.out.print(chiffres.length);
+        System.out.print(chiffres[0]);
+        System.out.print(chiffres[7]);
+        System.out.print(chiffres[10]);
+
+    }
+}
---
+++ src/tableaux/ExerciceTableauChaines.java
@@ -0,0 +1,16 @@
+package tableaux;
+
+public class ExerciceTableauChaines {
+    public static void main(String[] args) {
+
+        String[] noms = {"Marseille", "Paris", "Nantes", "Arles", "Séte"};
+
+        System.out.print(noms.length);
+
+
+        noms[0] = "Reims";
+        System.out.print(noms.length);
+
+
+        }
+}
